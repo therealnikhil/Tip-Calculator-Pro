67e821ad6008cdf996eddd3f8a14650295d75fed
Nikhil Nandkumar <nikhilfaips@gmail.com>
2015-12-28 12:49:24 +0300

    Initial Commit

---
+++ tipsCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipsCalculator
+//
+//  Created by Nikhil Nandkumar on 12/28/15.
+//  Copyright © 2015 Nikhil Nandkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
